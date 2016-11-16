ac6df5fe7bb4b687f9619dd662b6e235b66d90d1
lv.hang <289751112@qq.com>
2016-11-16 11:30:07 +0800

    create database study activity

---
+++ app/src/main/java/com/example/lvhang/androidstudyproject/DatabaseStudy/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.lvhang.androidstudyproject.DatabaseStudy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.lvhang.androidstudyproject.R;
+
+/**
+ * Created by lv.hang on 2016/11/16.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.database_study_main);
+    }
+
+
+}
